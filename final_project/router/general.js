4437d7885bd6da7645aba10a1600bf9ff4c97b97
Sarah Ammar <Sarah.ammar94.sa90@gmail.com>
2023-10-14 22:27:50 +0200

    reached task 7

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,18 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.query.username;
+  const password = req.query.password;
+
+  if (username && password) {
+    if (!isValid(username)) { 
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    } else {
+      return res.status(404).json({message: "User already exists!"});    
+    }
+  } 
+  return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
@@ -24,20 +34,33 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+const author = req.params.author;
+const searchName = author.replace('+', ' ');
+//const firstIndex = author.indexOf(' ');
+//const firstName = author.substring(0, firstIndex);
+//const lastName = author.substring(firstIndex, author.length);
+ for(i = 1; i <= 10; i++) {
+  if (books[i].author === searchName)
+  {res.send(books[i]);} 
+ }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title1 = req.params.title;
+const title = title1.replace('+', ' ');
+ for(i = 1; i <= 10; i++) {
+  if (books[i].title === title)
+  {res.send(books[i]);} 
+ }
+
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+isbn = req.params.isbn;
+    {res.send(books[isbn].reviews);} 
+   }
+);
 
 module.exports.general = public_users;
